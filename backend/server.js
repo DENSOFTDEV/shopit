877f6dbe9deb09d1ed670cf2bfec0f05e0a9e292
dennis githinji <dgithinji@techchimp.co.ke>
2021-04-20 22:01:19 +0300

    bug fix

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ process.on('uncaughtException', error => {
 })
 
 //setting up config file
-if (process.env.NODE_ENV === 'PRODUCTION') require('dotenv').dotenv.config({path: 'backend/config/config.env'});
+if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({path: 'backend/config/config.env'});
 
 //connecting to db
 connectDatabse();
